d858625eaea172a22c972e1eb5bc5b20cc61f50d
santiiago18 <scardaya@myuax.com>
2026-03-05 09:16:27 +0100

    test (Main) : Se registrar 3 nuevos alumnos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        CentroFP centroFP = new CentroFP(20);
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+        Alumno alumno1 = new Alumno(1, "Santiago", "DAM", 19);
+
+        Alumno alumno2 = new Alumno(2, "Daniela", "DAW", 19);
+
+        Alumno alumno3 = new Alumno(3, "Jhonatan", "ASIR", 19);
+
+        // Registrar Alumnos
+        System.out.println(centroFP.registrarAlumno(alumno1));
+        System.out.println(centroFP.registrarAlumno(alumno2));
+        System.out.println(centroFP.registrarAlumno(alumno3));
+
+
+    }
 }
