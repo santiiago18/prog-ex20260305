1fdaf024fa5aa5855266c0ef5a0fc013c1fcf9ab
santiiago18 <scardaya@myuax.com>
2026-03-05 09:32:33 +0100

    test (Main) : Se buscan los alumnos registrados y otro no registrado para verificar si funciona el sistema

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         CentroFP centroFP = new CentroFP(20);
 
-
         Alumno alumno1 = new Alumno(1, "Santiago", "DAM", 19);
 
         Alumno alumno2 = new Alumno(2, "Daniela", "DAW", 19);
 
         Alumno alumno3 = new Alumno(3, "Jhonatan", "ASIR", 19);
+
 
         // Registrar Alumnos
         System.out.println(centroFP.registrarAlumno(alumno1));
@@ -16,6 +16,10 @@ public class Main {
 
         //Mostrar alumnos
         System.out.println(centroFP.mostrarAlumnos());
+
+        //Buscar Alumnos
+        System.out.println(centroFP.buscarAlumno(2));
+        System.out.println(centroFP.buscarAlumno(4));
 
 
     }
