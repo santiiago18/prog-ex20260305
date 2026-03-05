6a05dc63b6cdbc4006b6707a0a2d3b6ae849104b
santiiago18 <scardaya@myuax.com>
2026-03-05 08:52:57 +0100

    feat (CentroFP) : Se crea el Constructor de CentroFP

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class CentroFP {
+    private Alumno [] alumnos;
+    private final int MAX_ALUMNOS;
+
+
+    /**
+     * Constructor de la clase CentroFP
+     * @param maximo_de_alumnos
+     */
+    public CentroFP(int maximo_de_alumnos){
+        MAX_ALUMNOS = maximo_de_alumnos;
+        alumnos = new Alumno[MAX_ALUMNOS];
+    }
+
+
+
+
 }
