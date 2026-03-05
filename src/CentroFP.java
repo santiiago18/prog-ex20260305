05254c572a02d7df48f5ca8fecd82a4eb14fb606
santiiago18 <scardaya@myuax.com>
2026-03-05 09:10:30 +0100

    feat (CentroFP) : Se el metodo mostrarAlumnos de CentroFP

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -10,6 +10,7 @@ public class CentroFP {
     public CentroFP(int maximo_de_alumnos){
         MAX_ALUMNOS = maximo_de_alumnos;
         alumnos = new Alumno[MAX_ALUMNOS];
+
     }
 
     /**
@@ -64,6 +65,23 @@ public class CentroFP {
         }
         return alumnoRegistado;
     }
+
+
+    /**
+     *Lista de los alumnos que estan registrados
+     */
+    public void mostrarAlumnos(){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < alumnos.length; i++){
+            if(alumnos[i] != null){
+                sb.append(String.format("Alumno %d: ", alumnos[i].getId()));
+                sb.append("\n");
+            }
+        }
+
+    }
+
+
 
 
 
